3a7a0cce49a6c71fb1bd421ad030c0e33697979b
Rose <Rose22@users.noreply.github.com>
2025-05-30 19:41:03 +0200

    fixed some bugs

--- install_vr.sh
+++ install_vr.sh
@@ -23,6 +23,7 @@ sleep 3
 
 # install custom scripts and configs
 mkdir $HOME/.vr_scripts
+mkdir $HOME/.vr_scripts/logs
 
 echo "$(cat <<EOF
 #!/bin/sh
@@ -40,12 +41,13 @@ export SAVED_AUDIO_SINK=\$(pactl get-default-sink)
 # switch to the wivrn audio output
 pactl set-default-sink wivrn.sink
 
-wlx-overlay-s --headless --openxr --log-to ~/.logs/wlx.log
+wlx-overlay-s --headless --openxr --log-to ~/.vr_scripts/logs/wlx.log
 
 # switch audio output back to the stored default
 pactl set-default-sink $SAVED_AUDIO_SINK
 EOF
 )" > $HOME/.vr_scripts/start_wlxoverlay.sh
+chmod +x $HOME/.vr_scripts/start_wlxoverlay.sh
 
 mkdir -p $HOME/.config/wivrn
 echo "$(cat <<EOF
