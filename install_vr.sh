fe8df5a459d16fbc0524e885698ecd22dd21153a
Rose <Rose22@users.noreply.github.com>
2025-06-01 03:12:56 +0200

    removed separate steam account
    causes too many problems with wivrn

--- install_vr.sh
+++ install_vr.sh
@@ -26,21 +26,20 @@ sudo systemctl enable NetworkManager
 announce "installing AMD GPU drivers.."
 sudo pacman --noconfirm -Sy mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon
 
-announce "adding steam user (seperate user ensures less chance of failure).."
+announce "enabling autologin for user.."
 sudo groupadd nopasswdlogin
-sudo useradd -m -G nopasswdlogin steam
-sudo bash -c "(echo steam; echo steam) | passwd steam"
+sudo useradd -m -G nopasswdlogin $(whoami)
 
 announce "installing vnc server for even more remote access.."
 sudo pacman --noconfirm -Sy tigervnc
 
-sudo -u steam bash -c "(echo steamvnc; echo steamvnc) | vncpasswd"
+(echo uservnc; echo uservnc) | vncpasswd
 cat <<EOF | sudo tee /etc/tigervnc/vncserver.users
-:1=steam
+:1=$(whoami)
 EOF
 
-sudo -u steam mkdir -p /home/steam/.config/tigervnc
-cat <<EOF | sudo -u steam tee /home/steam/.config/tigervnc/config
+mkdir -p $HOME/.config/tigervnc
+cat <<EOF | tee $HOME/.config/tigervnc/config
 session=i3
 geometry=1024x768
 alwaysshared
@@ -66,7 +65,7 @@ sudo pacman --noconfirm -Sy sway waybar swaync swaybg swaylock swayimg swayidle
 
 sudo systemctl enable vncserver@:1
 
-announce "installed VNC, you can now log in remotely as steam to use graphical applications!"
+announce "installed VNC, you can now log in remotely to use graphical applications!"
 
 announce "installing paru.."
 sudo pacman --noconfirm -Sy --needed base-devel git rust
@@ -76,7 +75,7 @@ makepkg --noconfirm -si
 cd ..
 
 announce "installing emptty display manager.."
-sudo paru --noconfirm -S emptty
+sudo paru --noconfirm -Sy emptty
 
 announce "writing emptty config.."
 cat <<EOF | sudo tee /etc/emptty/conf
@@ -93,7 +92,7 @@ PRINT_ISSUE=true
 PRINT_MOTD=false
 
 # Preselected user, if AUTOLOGIN is enabled, this user is logged in.
-DEFAULT_USER=steam
+DEFAULT_USER=$(whoami)
 
 # Enables Autologin, if DEFAULT_USER is defined and part of nopasswdlogin group. Possible values are "true" or "false".
 AUTOLOGIN=true
@@ -176,15 +175,14 @@ announce "installing wivrn, wlx-overlay-s and wayvr-dashboard.."
 sudo pacman --noconfirm -Sy avahi
 sudo systemctl enable --now avahi-daemon
 
-paru --noconfirm -S wivrn-dashboard wlx-overlay-s-git wayvr-dashboard-git
+paru --noconfirm -Sy wivrn-dashboard wlx-overlay-s-git wayvr-dashboard-git
 # manually enable the service because using systemctl over sudo doesn't work well without very weird workarounds
-sudo -u steam mkdir -p /home/steam/.config/systemd/user/default.target.wants
-sudo -u steam ln -s /usr/lib/systemd/user/wivrn.service /home/steam/.config/systemd/user/default.target.wants/wivrn.service
+sudo systemctl --user enable wivrn
 
 # install custom scripts and configs
-sudo -u steam mkdir /home/steam/.scripts
+mkdir $HOME/.scripts
 
-cat << EOF | sudo -u steam tee /home/steam/.scripts/start_wlxoverlay.sh
+cat << EOF | tee $HOME/.scripts/start_wlxoverlay.sh
 #!/bin/sh
 
 # remove the gamescope performance overlay (it can mess with things)
@@ -196,13 +194,13 @@ export WAYLAND_DISPLAY=gamescope-0
 
 wlx-overlay-s --headless --openxr
 EOF
-sudo -u steam chmod +x /home/steam/.scripts/start_wlxoverlay.sh
+chmod +x $HOME/.scripts/start_wlxoverlay.sh
 
-sudo -u steam mkdir -p /home/steam/.config/wivrn
-cat <<EOF | sudo -u steam tee /home/steam/.config/wivrn/config.json
+mkdir -p $HOME/.config/wivrn
+cat <<EOF | tee $HOME/.config/wivrn/config.json
 {
   "application": [
-	  "/home/steam/.scripts/start_wlxoverlay.sh"
+	  "$HOME/.scripts/start_wlxoverlay.sh"
   ]
 }
 EOF
@@ -218,11 +216,10 @@ Done! Now reboot your system, and you should immediately be logged into steamOS 
 From now on, you can remote into your system using ssh for console access, and vnc for graphical access.
 Feel free to uninstall tigervnc if you don't want this!
 
-A new user called steam has been created, and the password for it is steam. This keeps steam seperate from your main user account, in case you want to use this computer for other things too as your normal user account.
-You can change the steam user password if you wish, it has no effect on auto login.
 
-You can access VNC through port 5901, the password is steamvnc. This'll log you into a remote desktop session in the steam user account.
+You can access VNC through port 5901, the password is uservnc. This'll log you into a remote desktop session in your user account.
 You can use this to do stuff like add non-steam games to steam, install software needed under the steam account such as decky-loader, etc.
+Please change your vnc password asap, it's insecure when set to the default.
 
 Wivrn should also have been enabled, and on your quest, you should be able to connect!
 Open wivrn-dashboard as the steam user in order to begin the wivrn pairing process. You can either do this using a mouse on your PC, or through VNC.
